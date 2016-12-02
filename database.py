63469cda7d34bac128d3c47c4d092edee6889658
Tom Walsh <tom.walsh@delphix.com>
2016-12-02 15:46:49 -0500

    best stops for location

--- database.py
+++ database.py
@@ -12,11 +12,29 @@ class Database:
     def get_all_stops(self):
         return self.stops
 
+    def init_sorted_lat_stops(self):
+        self.stops_by_lat = []
+        for stop in self.stops:
+            self.stops_by_lat.append({'id': stop, 'lat': self.stops[stop]['lat']})
+        self.stops_by_lat.sort(key = lambda item: item['lat'])
+
+    def init_sorted_lon_stops(self):
+        self.stops_by_lon = []
+        for stop in self.stops:
+            self.stops_by_lon.append({'id': stop, 'lon': self.stops[stop]['lon']})
+        self.stops_by_lon.sort(key = lambda item: item['lon'])
+
+    def __init__(self, routes, stops):
+        self.routes = routes
+        self.stops = stops
+        self.init_sorted_lat_stops()
+        self.init_sorted_lon_stops()
+
+
 def create_from_static_data():
-    db = Database()
-    db.routes = read_json_file('static_data/bus_routes.json')
-    db.stops = read_json_file('static_data/bus_stops.json')
-    return db
+    return Database(
+        read_json_file('static_data/bus_routes.json'),
+        read_json_file('static_data/bus_stops.json'))
 
 def add_to_stops(stop_list, more_stops):
     for stop in more_stops:
@@ -27,12 +45,9 @@ def add_to_stops(stop_list, more_stops):
 
 def create_from_mbta_server():
     server = MBTAServer();
-    db = Database()
     routes = server.get_all_routes()
     all_stops = {}
     for route in routes:
         route_stops = server.get_stops_for_route(route)
         add_to_stops(all_stops, route_stops)
-    db.route = routes
-    db.stops = all_stops
-    return db
+    return Database(routes, all_stops)
--- main.py
+++ main.py
@@ -1,5 +1,88 @@
 import database
+import math
+
+# Code modified from the bisect module to allow searching based on a field
+def bisect_left(array, value, key):
+    lo = 0
+    hi = len(array)
+    while lo < hi:
+        mid = (lo+hi)//2
+        if array[mid][key] < value: lo = mid+1
+        else: hi = mid
+    return lo
+
+def bisect_right(array, value, key):
+    lo = 0
+    hi = len(array)
+    while lo < hi:
+        mid = (lo+hi)//2
+        if value < array[mid][key]: hi = mid
+        else: lo = mid+1
+    return lo
+
+# For a sorted array, returns a slice of objects o such that min <= o[key] <= max
+def get_subinterval(array, min, max, key):
+    smallest = bisect_left(array, min, key)
+    if smallest == len(array):
+        return []
+
+    largest = bisect_right(array, max, key)
+    if not largest:
+        return []
+
+    return array[smallest:largest]
+
+def get_ids(array):
+    return frozenset(v['id'] for v in array)
+
+# Returns all stops within the given distance from the given point
+# For simplicity, this code searches a bounding square, not a circle.
+def get_stops_near_location(db, lat, lon, radius):
+    lat_rad = radius / 69 # 69 miles per degree
+    lon_rad = radius / 51 # 51 miles per degree near Boston
+
+    matching_lats =  get_ids(get_subinterval(db.stops_by_lat, lat - lat_rad, lat + lat_rad, 'lat'))
+    matching_lons =  get_ids(get_subinterval(db.stops_by_lon, lon - lon_rad, lon + lon_rad, 'lon'))
+    matching = matching_lats.intersection(matching_lons)
+    stops = {}
+    for id in matching:
+        stops[id] = db.stops[id]
+    return stops
+
+# Returns distance in tenths of miles (about one large block)
+def get_distance(lat1, lon1, lat2, lon2):
+    dy = (lat2-lat1) * 690  # 69 miles per degree
+    dx = (lon2-lon1) * 510  # 51 miles per degree near Boston
+    return math.sqrt(dy*dy + dx*dx)
+
+# For each route/direction in the given area, returns the outset closest to the given point
+def get_outsets_for_location(db, lat, lon, radius):
+    best_outsets = {}
+    stops = get_stops_near_location(db, lat, lon, radius)
+
+    # This does a brute force O(n) search, assuming that the stop list is relatively small
+    for stop in stops:
+        stoplat = stops[stop]['lat']
+        stoplon = stops[stop]['lon']
+        distance = get_distance(lat, lon, stoplat, stoplon)
+        for route in stops[stop]['routes']:
+            route_id = route['route']
+            direction_id = route['direction']
+            if route_id not in best_outsets:
+                best_outsets[route_id] = {}
+            if direction_id not in best_outsets[route_id]:
+                best_outsets[route_id][direction_id] = {}
+            if distance not in best_outsets[route_id][direction_id] or best_outsets[route_id][direction_id]['distance'] > distance:
+                best_outsets[route_id][direction_id]['distance'] = distance
+                best_outsets[route_id][direction_id]['stop_id'] = stop
+    return best_outsets
+
+
 
 #db = database.create_from_static_data()
 db = database.create_from_mbta_server()
-print db.stops
+
+lat = 42.379583
+lon = -71.095352
+
+print get_outsets_for_location(db, lat, lon, 0.25)
